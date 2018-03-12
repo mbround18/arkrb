d25c96d3f21b37c4182e856e88b44a4ba93f50b9
Michael Bruno <michael.bruno1337@gmail.com>
2018-03-12 02:36:56 -0700

    Updating some Spec tests

--- test/unit/arkrb/server_test.rb
+++ test/unit/arkrb/server_test.rb
@@ -2,8 +2,8 @@ require 'arkrb/server'
 require_relative '../../test_helper'
 class ArkrbServer < Arkrb::Server
 
-  def arkmanager_exec(command)
-    command
+  def arkmanager_exec(command, args = [])
+    [command, args]
   end
 
 end
@@ -22,82 +22,82 @@ class ServerTest < TestHelper
   end
 
   def test_install
-    assert_equal 'install', @server.install
+    assert_includes @server.install, :install
   end
 
   def test_start!
-    assert_equal 'start', @server.start!
+    assert_includes @server.start!, :start
   end
 
 
   def test_stop!
-    assert_equal 'stop', @server.stop!
+    assert_includes @server.stop!, :stop
   end
 
 
   def test_restart!
-    assert_equal 'restart', @server.restart!
+    assert_includes @server.restart!, :restart
   end
 
 
   def test_update!
-    assert_equal 'update', @server.update!
+    assert_includes @server.update!, :update
   end
 
 
   def test_backup!
-    assert_equal 'backup', @server.backup!
+    assert_includes @server.backup!, :backup
   end
 
 
   def test_save_world!
-    assert_equal 'saveworld', @server.save_world!
+    assert_includes @server.save_world!, :save_world
   end
 
 
   def test_enable_mod
-    assert_equal 'enablemod 12345678', @server.enable_mod(12345678)
+    assert_equal [:enable_mod, 12345678], @server.enable_mod(12345678)
   end
 
 
   def test_disable_mod
-    assert_equal 'disablemod 12345678', @server.disable_mod(12345678)
+    assert_equal [:disable_mod, 12345678], @server.disable_mod(12345678)
   end
 
 
   def test_install_mod
-    assert_equal 'installmod 12345678', @server.install_mod(12345678)
+    assert_equal [:install_mod, 12345678], @server.install_mod(12345678)
 
   end
 
 
   def test_uninstall_mod
-    assert_equal 'uninstallmod 12345678', @server.uninstall_mod(12345678)
+    assert_equal [:uninstall_mod, 12345678], @server.uninstall_mod(12345678)
   end
 
 
   def test_reinstall_mod
-    assert_equal 'reinstallmod 1234568', @server.reinstall_mod(1234568)
+    assert_equal [:reinstall_mod, 1234568], @server.reinstall_mod(1234568)
   end
 
 
   def test_broadcast
-    assert_equal "broadcast 'Hello World!'", @server.broadcast('Hello World!')
+    assert_equal [:broadcast, "Hello World!"], @server.broadcast('Hello World!')
   end
 
   # @return [Object]
   def test_rcon_cmd
-    assert_equal 'rcon test', @server.rcon_cmd('test')
+    assert_equal [:rcon, "test"], @server.rcon_cmd('test')
   end
 
   # @return [Boolean]
   def test_update_available?
-    assert_equal 'checkupdate', @server.update_available?
+    assert_equal [:check_update, []], @server.update_available?
   end
 
   # @return [Boolean]
   def test_mod_updates_available?
-    assert_equal 'checkmodupdate', @server.mod_updates_available?
+    assert_equal [:check_mod_update, []], @server.mod_updates_available?
   end
 
 end
--- test/unit/arkrb_test.rb
+++ test/unit/arkrb_test.rb
@@ -12,4 +12,23 @@ class ArkrbTest < TestHelper
     version.each { |n| assert_match /^[0-9]*$/, n }
   end
 
+  def test_bash_exec_found
+    refute_nil Arkrb::BASH_EXEC
+    assert File.exist?(Arkrb::BASH_EXEC)
+  end
+
+  def test_curl_exec_found
+    refute_nil Arkrb::CURL_EXEC
+    assert File.exist?(Arkrb::CURL_EXEC)
+  end
+
+  def test_user_home_found
+    refute_nil Arkrb::USER_HOME
+    assert File.exist?(Arkrb::USER_HOME)
+  end
+
+  def test_ark_server_tools_md5_is_string
+    refute_nil Arkrb::ARK_SERVER_TOOLS_MD5
+  end
+
 end
